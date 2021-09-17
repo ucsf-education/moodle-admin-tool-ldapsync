f5dae4c58e378fad500c3db72b72fd2349b38ac4
Carson Tam <carson.tam@ucsf.edu>
2021-09-17 11:18:45 -0700

    Updated version.php.

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019031804; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2019031805; // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2018051700; // Requires this Moodle version
 $plugin->component = 'tool_ldapsync'; // Full name of the plugin (used for diagnostics)
