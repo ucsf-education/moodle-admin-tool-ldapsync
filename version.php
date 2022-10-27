679ba87d2614109d265dfe0f4f018681700d13b5
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-10-27 11:59:04 -0700

    bumps miminum required moodle version to v4, updates plugin version.

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021101900; // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2018051700; // Requires this Moodle version
+$plugin->version   = 2022102700; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2022041200; // Requires this Moodle version
 $plugin->component = 'tool_ldapsync'; // Full name of the plugin (used for diagnostics)
