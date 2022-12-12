4cfb4d0cd3ef26d211f8d1e829933c33fe25ef6f
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-12-12 15:35:52 -0800

    version and requirement bump to Moodle 4.1

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022102700; // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2022041200; // Requires this Moodle version
+$plugin->version   = 2022121200; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2022111800; // Requires this Moodle version
 $plugin->component = 'tool_ldapsync'; // Full name of the plugin (used for diagnostics)
